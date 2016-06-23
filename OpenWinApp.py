75ceacff13d889da981d875da23ca3c8c2931eb4
ejiaybn <yanbin.jia@ericsson.com>
2016-06-23 17:13:50 +0800

    Add others application, browser, to xshell

--- OpenWinApp.py
+++ OpenWinApp.py
@@ -1,9 +1,33 @@
 import subprocess;
+import webbrowser;
 
 #Open the total commander application#
-ps1 = subprocess.Popen(r'"e:\download\totalcmd\TOTALCMD64.EXE"')
-ps1.wait
+subprocess.Popen("c:\Program Files (x86)\Microsoft Office\Office14\OUTLOOK.EXE")
 
 #Open the outlook application#
-ps2 = subprocess.Popen(r'"c:\Program Files (x86)\Microsoft Office\root\Office16\ONENOTE.EXE"')
-ps2.wait
+#subprocess.call("c:\\totalcmd\TOTALCMD64.EXE")
+
+#Open the OneNote application#
+subprocess.Popen("c:\Program Files (x86)\Microsoft Office\Office14\ONENOTE.EXE")
+
+#Open the Sublime Text3 application#
+subprocess.Popen("c:\DISK D\download\Sublime3\sublime_text.exe")
+
+#Open the prefered website on default browser#
+
+webbrowser.open_new_tab("http://sports.sina.com.cn")
+webbrowser.open_new_tab("http://www.ximalaya.com/zhubo/1412917/")
+webbrowser.open_new_tab("http://calstore.internal.ericsson.com/alex")
+webbrowser.open_new_tab("http://taskboard.es.eu.ericsson.se:3000/taskboard/show/533")
+
+
+#Open the Xshell application#
+
+
+subprocess.Popen("c:\Program Files (x86)\NetSarang\Xshell 5\Xshell.exe")
+#subprocess.Popen(["C:\Program Files (x86)\NetSarang\Xshell 5\xshell","c:\Users\ejiaybn\Documents\NetSarang\Xshell\Sessions\NIV\NIV-Client.xsh"])
+
+
+
+
+
